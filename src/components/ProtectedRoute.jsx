3a3051a4db057cfd5735475f4e4afc28a7d780c0
Kaushik Nitin Naik <kaushik.naik1602@gmail.com>
2023-12-23 01:17:55 +0530

    Add URL to package.json

--- src/components/ProtectedRoute.jsx
+++ src/components/ProtectedRoute.jsx
@@ -3,7 +3,7 @@ import React, { useState } from "react";
 import { Outlet, Navigate } from "react-router-dom";
 
 const ProtectedRoute = ({ component: Component, isAuthenticated, ...rest }) => {
-  return !isAuthenticated ? <Outlet {...rest} /> : <Navigate to="/" />;
+  return !isAuthenticated ? <Outlet {...rest} /> : <Navigate to="/task-4" />;
 };
 
 export default ProtectedRoute;
