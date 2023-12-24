1e72fc843704fbe867b1edf15c8c9b70a722594f
Kaushik Nitin Naik <kaushik.naik1602@gmail.com>
2023-12-24 19:24:57 +0530

    Minor Changes

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,15 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { resolve } from "path";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/task-4/",
+  base: "/task-4/", // Replace with your GitHub repository name
+  rollupOptions: {
+    input: {
+      main: resolve(__dirname, "index.html"),
+      dashboard: resolve(__dirname, "dashboard.html"),
+    },
+  },
   plugins: [react()],
 });
